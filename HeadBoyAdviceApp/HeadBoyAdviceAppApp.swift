a0a843df5a47193d27361c457569da6b4828bb43
Benjamn <bfreeman@lcs.on.ca>
2021-03-03 11:30:12 -0500

    Initial Commit

---
+++ HeadBoyAdviceApp/HeadBoyAdviceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadBoyAdviceAppApp.swift
+//  HeadBoyAdviceApp
+//
+//  Created by Ben Freeman on 2021-03-03.
+//
+
+import SwiftUI
+
+@main
+struct HeadBoyAdviceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
