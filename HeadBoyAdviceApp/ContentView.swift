47a190c63290754506b2c7b549977b63afc39058
Benjamn <bfreeman@lcs.on.ca>
2021-03-11 16:53:02 -0500

    Info view added. The view isn't finished yet put it has generally all of the main functionality that I want

--- HeadBoyAdviceApp/ContentView.swift
+++ HeadBoyAdviceApp/ContentView.swift
@@ -10,14 +10,17 @@ import SwiftUI
 struct ContentView: View {
     @State var searchInput = ""
     @State var next = false
-    
+    @State var info = false
     var body: some View {
+
         if next == true {
             VStack {
                 SearchBarView(text: $searchInput)
                     .padding(.top)
                 Spacer()
-                
+
+                Spacer()
+            
                 if searchInput == "" {
                     
                     Spacer()
@@ -31,10 +34,10 @@ struct ContentView: View {
                     Spacer()
                     Spacer()
                     Button("Info...") {
-                        
+                        self.info.toggle()
                     }
                     .padding(.bottom)
-                    
+                
                 } else {
                     
                 }
@@ -42,11 +45,48 @@ struct ContentView: View {
                 
             }
         } else {
-            WelcomScreen()
+            VStack {
+                Image("Image-1")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("Welcom to the LCS head boy advice app!")
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.all)
+                
+                Text("This app is designed to help students with questions they may have about the school rules, what to do in certain situations, and more! Click on the Info button to learn more and the next button to continue ")
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+                    .padding([.leading, .bottom, .trailing])
             }
-            
+            Spacer()
+
+            HStack{
+                
+                Button("Info..."){
+                    self.info.toggle()
+                }
+                .padding([.leading, .bottom])
+                Spacer()
+                Button("Next") {
+                    next = true
+                }
+                .padding([.bottom, .trailing])
+            }
         }
+        
+         if info == true {
+            InfoView()
+        }
+        
+        
     }
+
+}
+            
+
+    
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
