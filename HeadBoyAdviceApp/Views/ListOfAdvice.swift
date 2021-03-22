ed1a6e0e25c9f1c303fb44345918feec3b39c3a7
Benjamn <bfreeman@lcs.on.ca>
2021-03-22 11:47:15 -0400

    List array created(Still needs more detail)

---
+++ HeadBoyAdviceApp/Views/ListOfAdvice.swift
@@ -0,0 +1,27 @@
+//
+//  ListOfAdvice.swift
+//  HeadBoyAdviceApp
+//
+//  Created by Ben Freeman on 2021-03-22.
+//
+
+import SwiftUI
+
+struct HBAD: Identifiable {
+    let id = UUID()
+    let title: String
+    let advice: String
+
+}
+let advice = [
+    HBAD (title: "What to do if you fail you L-CAT", advice: "Don't panic, you're not in trouble. You should first talk yo your head of house (Same for day students) and follow there instuctions. They will likley have to quarantine with your house. You should also contact you teachers you have for that class to make sure they give you a zoom link and are aware of what's going on so you don't get marked late."),
+    HBAD (title: "How to ask the teacher to go to the washroom", advice: """
+        This is a common situaton. (I too have been through this) ;) just remember that your teachers are very nice and will likley be understanding.
+
+        HEADS UP!!!
+       If your in a test or near the end of class you may be asked to wait.
+"""
+    )
+        
+    
+]
